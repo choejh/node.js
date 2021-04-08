61402a1040f99d583bf8b5725f9d0aad48525ab2
choejh <71997987+choejh@users.noreply.github.com>
2021-04-08 14:06:34 +0900

    express_router

--- js/server_side_directory/app.js
+++ js/server_side_directory/app.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-    res.send('hello home page');
-}); 
+//정적인 파일이 위치할 디렉토리 지정
+app.use(express.static('public'));
+
 //사용자가 웹서버에 접속할때 get/post방식으로 접속
 //일반적으로 url접속은 get방식!
 //get방식 접속 사용자 2번째 인자로 전달한 함수가 실행되도록 약속
 //get(~) <- 라우터라 말함 (사용자의 요청과 처리인 controller를 중개시켜주는 역할)
+
+
+
+app.get('/',function(req,res){
+    res.send('hello home page');
+}); 
+
+app.get('/route',function(req,res){
+    res.send('hello router, <img src="/panda.jpg">')}
+);
 
 app.get('/login',function(req,res){
     res.send('Login please');
