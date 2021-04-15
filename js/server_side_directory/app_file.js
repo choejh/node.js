cc7932d63c2104d80fb17aaf7b41f800cda5d6e7
choejh <71997987+choejh@users.noreply.github.com>
2021-04-15 19:56:19 +0900

    final

--- js/server_side_directory/app_file.js
+++ js/server_side_directory/app_file.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 
 var fs = require('fs');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 app.locals.pretty = true;
 
@@ -12,19 +13,58 @@ app.set('views','./views_file');
 app.set('view engine','jade');
 
 app.get('/topic/new', function(req,res){
-    res.render('new');
-});
-
-app.get('/topic', function(req,res){
     fs.readdir('data',function(err,files){
         if(err){
             console.log(err);
             res.status(500).send('Internal Server Error');
         }
-        res.render('view',{topics:files});
-         })  
+        res.render('new',{topics:files});
+    });
 });
 
+app.get(['/topic','/topic/:id'], function(req,res){
+    fs.readdir('data',function(err,files){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        var id = req.params.id;
+        if(id){
+        //id값이 있을때
+        fs.readFile('data/'+id,'utf-8', function(err,data){
+            if(err){
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            }
+        res.render('view',{topics:files, title:id, description:data})
+        })
+    } else {
+        //id값이 없을 때
+        res.render('view',{topics:files,title:'Welcome',description:'hello~'}); 
+        } 
+    })
+
+});
+/*
+app.get('/topic/:id',function(req,res){
+    var id = req.params.id;
+
+    fs.readdir('data',function(err,files){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+
+        fs.readFile('data/'+id,'utf-8', function(err,data){
+            if(err){
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            }
+        res.render('view',{topics:files, title:id, description:data})
+        })
+     }) 
+})
+*/
 
 app.post('/topic', function(req,res){
    var title = req.body.title;
@@ -33,7 +73,7 @@ app.post('/topic', function(req,res){
     if(err){
         res.status(500).send('Internal Server Error');
     }
-    res.send('success~!');
+    res.redirect('/topic/'+title);
    });
 })
 app.listen(3000, function(){
